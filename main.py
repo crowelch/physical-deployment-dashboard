eb90b9c4dae271ef3d538c9bc26f3d2e3f2955c0
Chris Crowell <crowelch@mail.uc.edu>
2017-02-07 21:19:08 -0500

    use while loop instead of listening for events

--- main.py
+++ main.py
@@ -16,25 +16,22 @@ KEY_SWITCH = 21
 DEPLOY_BUTTON = 20
 
 ### Setup Channels ###
-# Setup switches, defaulting to low #
+# Setup switches, defaulting to low
 GPIO.setup(SWITCH_ONE, GPIO.IN, initial=GPIO.LOW)
 
 # Setup control inputs, defaulting to low
 GPIO.setup(KEY_SWITCH, GPIO.IN, initial=GPIO.LOW)
 GPIO.setup(DEPLOY_BUTTON, GPIO.IN, initial=GPIO.LOW)
 
-# Setup interrupt callbacks
-def test_callback(channel):
-    print("Rising edge detected on " + channel)
-
-def button_callback:
-    # Ensure controller is armed
-    if GPIO.input(KEY_SWITCH):
+while True:
+    # Only proceed if dashboard is armed
+    if (GPIO.input(DEPLOY_BUTTON) and GPIO.input(KEY_SWITCH)):
         # Check which (if any) switches are enabled
         enabled_switches = filter(GPIO.input, switches)
         if len(enabled_switches) > 0:
             for switch in enabled_switches:
                 ## Deploy
+                print('deploying ' + switch)
 
-## Attach events to callbacks
-GPIO.add_event_detect(DEPLOY_BUTTON, GPIO.RISING, button_callbacks)
+
+    time.sleep(0.01)
